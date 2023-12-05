06da54db33d64bbe402ea6425466c26eb6e9bc43
arseny <arsenyxiii@gmail.com>
2023-12-05 15:45:04 +0300

    add task

--- README.md
+++ README.md
@@ -12,3 +12,29 @@
 если значение минут < 30, то таск скипается
 иначе выводит дату и завершается
 регламент запуска дага задаем в формате cron
+
+# Зависимости
+Install venv
+```
+python3.10 -m venv venv
+source venv/bin/activate
+```
+
+Установить распложение airflow в текущей папке + установить airflow
+```
+export AIRFLOW_HOME="$(pwd)"
+AIRFLOW_VERSION=2.7.3
+
+# Extract the version of Python you have installed. If you're currently using a Python version that is not supported by Airflow, you may want to set this manually.
+# See above for supported versions.
+PYTHON_VERSION="$(python --version | cut -d " " -f 2 | cut -d "." -f 1-2)"
+
+CONSTRAINT_URL="https://raw.githubusercontent.com/apache/airflow/constraints-${AIRFLOW_VERSION}/constraints-${PYTHON_VERSION}.txt"
+# For example this would install 2.7.3 with python 3.8: https://raw.githubusercontent.com/apache/airflow/constraints-2.7.3/constraints-3.8.txt
+
+pip install "apache-airflow==${AIRFLOW_VERSION}" --constraint "${CONSTRAINT_URL}"
+```
+
+Запустить airflow
+
+`airflow standalone`
---
+++ dags/task1_dag.py
@@ -0,0 +1,46 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+
+args = {
+    'owner': 'airflow',
+    'start_date': datetime(2023, 12, 5,),
+}
+
+dag = DAG(
+    dag_id='task_1', default_args=args,
+    schedule_interval="*/15 * * * *",)
+
+
+def push_current_date_time(ds, **kwargs) -> datetime:
+    """Вернуть текущее дату-время."""
+    current_date_time = datetime.now()
+    return current_date_time
+
+
+first_task = PythonOperator(
+    task_id='push_current_date_time',
+    provide_context=True,
+    python_callable=push_current_date_time,
+    dag=dag)
+
+
+def print_date_or_skip(ds, ti, **kwargs) -> str:
+    """Получение даты или скип."""
+    current_datetime = ti.xcom_pull(task_ids='push_current_date_time')
+    if current_datetime.minute >= 30 :
+        print(current_datetime.date())
+        return 'Finish'
+    return 'Skip minute < 30'
+        
+
+second_task = PythonOperator(
+    task_id='print_date_or_skip',
+    provide_context=True,
+    python_callable=print_date_or_skip,
+    dag=dag)
+
+
+first_task >> second_task
